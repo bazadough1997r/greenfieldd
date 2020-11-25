858eb5b77ff595a74a6e5751004fc176bbf11122
Fedaa <fadooshjad2001@gmail.com>
2020-11-25 19:25:00 +0200

    stock database created

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
-var express = require('express')
-var  app = express()
+const express = require('express')
+const dbMySQL = require('../database-sql/index')
+const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
