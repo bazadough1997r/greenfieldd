a8058d3c8680cb1b647d8988bf639ad1f4156ab7
= <=>
2020-11-25 19:04:59 +0200

    db

--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const myDB = require('../database-sql/index.js')
 const app = express()
 const port = 3000
 
@@ -7,5 +8,5 @@ app.get('/', (req, res) => {
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+  console.log(`Server listening at http://localhost:${port}`)
 })
