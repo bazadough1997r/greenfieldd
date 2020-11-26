68b8fee605de648feb8ccc6c1b7ea278296dd2da
abeer-rawash <abeer.rawa96@gmail.com>
2020-11-26 09:08:02 +0200

    unwanted version

--- database-sql/index.js
+++ database-sql/index.js
@@ -1,40 +1,35 @@
-// const mysql = require('mysql');
-
-// // First you need to create a connection to the database
-// // Be sure to replace 'user' and 'password' with the correct values
-// const con = mysql.createConnection({
-//   host: 'localhost',
-//   user: 'abeer',
-//   password: '0000',
-//   // database: "cars",
-// });
-
-// con.connect((err) => {
-//   if(err){
-//     console.log('Error connecting to Db');
-//     return;
-//   }
-//   console.log('Connection established');
-// });
-
-// con.end((err) => {
-//   // The connection is terminated gracefully
-//   // Ensures all remaining queries are executed
-//   // Then sends a quit packet to the MySQL server.
-// });
-
-
 var mysql = require('mysql');
+var data = require('./data.json')
+
+// var data = JSON.parse(body);
+// var responseJson = JSON.stringify(data.response);
+
+
 var con = mysql.createConnection({
 host: "localhost",
 user: "abeer",
-password: "0000"
+password: "0000",
+database: "stock"
 });
+
+
+
 con.connect(function(err) {
-if (err) throw err;
-console.log("Connected!");
+  if (err) throw err;
+  console.log("Connected!");
+
 con.query("CREATE DATABASE IF NOT EXISTS stock", function (err, result) {
-if (err) throw err;
-console.log("Database created");
+ if (err) throw err;
+ console.log("Database created");
+
+  // var Cars = "CREATE TABLE IF NOT EXISTS cars (brand VARCHAR(20), year VARINTEGER(4),  price VARsmallmoney,description VARCHAR(250), img VARimage)";
+  // con.query(Cars, function (err, result) {
+  // if (err) throw err;
+  // console.log("Table created");
+
+  });
 });
-});
+
+
+
+
