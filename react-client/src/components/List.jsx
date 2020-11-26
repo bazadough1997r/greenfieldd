47274c4e6b565538d88636ea93adfde040e6558b
abeer-rawash <abeer.rawa96@gmail.com>
2020-11-26 16:26:00 +0200

    filters done-working on rendering

---
+++ react-client/src/components/List.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+class List extends React.Component {
+  //ONCLICK EVENT > POST REQUEST
+  constructor(props) {
+    super(props);
+  }
+
+
+render(){
+  return (
+    <div>
+      {
+        this.props.cars.map(car=>(
+        <li key = {car._id}>
+        <h3>{car.brand}</h3>
+        <p> {car.year}</p>
+        <p> {car.price}</p>
+        <p> {car.description}</p>
+        <p> {car.img}</p>
+        </li>))
+      }
+    </div>
+  )}
+}
+export default List;
