20323460511cb6b0897f6f77beb92a37b99c1f4b
= <=>
2020-11-26 14:02:04 +0200

    dummy data inserted to cars table

--- database-sql/index.js
+++ database-sql/index.js
@@ -23,15 +23,14 @@ con.query(Cars, function (err, result) {
 
 
 //save function to see our dummy data in the mysql terminal (insert data in the columns)
-
-  for (var i = 0; i < data.length; i++) {
-    var inserting = `REPLACE INTO cars (brand, year, price, description, image,id) VALUES (?, ?, ?, ?, ?,?) `;
-    let rows = [data[i].brand, data[i].year, data[i].price, data[i].description,data[i].image,i+1];
-    con.query(inserting, rows, function (err, results, fields) {
-      if (err) throw err;
-      console.log("Table inserted");
-      });
-  }
+for (var i = 0; i < data.length; i++) {
+  var inserting = `REPLACE INTO cars (brand, year, price, description, image,id) VALUES (?, ?, ?, ?, ?,?) `;
+  let rows = [data[i].brand, data[i].year, data[i].price, data[i].description,data[i].image,i+1];
+  con.query(inserting, rows, function (err, results, fields) {
+    if (err) throw err;
+    console.log("Table inserted");
+    });
+}
 
 
 
