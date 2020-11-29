e25ba99046d6ad252ac1fa57c55d2826687a1bd0
Fedaa <fadooshjad2001@gmail.com>
2020-11-29 09:31:34 +0200

    slide of photo

--- react-client/src/components/logotext.jsx
+++ react-client/src/components/logotext.jsx
@@ -3,14 +3,21 @@ import Typography from '@material-ui/core/Typography';
 import { styled } from '@material-ui/core/styles';
 const LogoText = () => {
   var MyText = styled(Typography)({
-    variant: "h3",
-     margin: " -5px 90px 0px 400px",
+
+     margin: " -5px 100px 0px 400px",
+     fontWeight: 500,
+      fontSize: 50,
+    //  fontWeight : 20
     // marginTop : 100
+    //variant="h2"
   })
   return(
-    <MyText  component="h2" gutterBottom>
-      SELL ONE BUY ANOTHER
+    <MyText  gutterBottom>
+       SELL ONE BUY ANOTHER
       </MyText>
+    // <MyText variant="h2" gutterBottom>
+    //   SELL ONE BUY ANOTHER
+    //   </MyText>
   )
 }
 export default LogoText;
