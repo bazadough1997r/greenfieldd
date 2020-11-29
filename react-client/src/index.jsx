e520298d033dc31f73429ffdddf1dbe65cafdbc8
Rawan Bazadough <bazadough1997@gmail.com>
2020-11-29 17:01:28 +0200

    login and sign up components ready

--- react-client/src/index.jsx
+++ react-client/src/index.jsx
@@ -2,7 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import $ from 'jquery';
 import About from './components/about.jsx';
-import Footer from './components/footer.jsx'
+import Footer from './components/footer.jsx';
+// import Login from './components/Login.jsx';
+// import Signup from './components/Signup.jsx';
 import Box from '@material-ui/core/Box' ;
 import LogoText from './components/logotext.jsx'
 import { styled } from '@material-ui/core/styles';
@@ -80,6 +82,8 @@ class App extends React.Component {
       <div>
       <span><img src = "https://scontent.famm5-1.fna.fbcdn.net/v/t1.0-9/128255422_227091088758997_7058702321390752154_n.jpg?_nc_cat=108&ccb=2&_nc_sid=730e14&_nc_eui2=AeEqFQYaN_ad4f9gK-R_00w0xeq3jpBC8OPF6reOkELw4wT-RH0yveh5W6rCgR4sTojyscWbVB4AC485fRaU9tmG&_nc_ohc=ige5aDJwuj0AX_nHzs2&_nc_oc=AQmh70KqaJc_XuRjsXfrEZ2TFCRNhUgAlWxjabqZ2UOrLvolXf7W1N34wTttPuSPf14&_nc_ht=scontent.famm5-1.fna&oh=aaea198d1eb4f019d2c150895411b2c1&oe=5FE68806"  width="250" height="125"></img><LogoText/></span>
       < About/><br></br>
+      {/* <Signup/><br></br>
+      <Login/><br></br> */}
         <Search
         onSubmitB = {this.submitBrandHandler.bind(this)}
         onSubmitY = {this.submitYearHandler.bind(this)}
