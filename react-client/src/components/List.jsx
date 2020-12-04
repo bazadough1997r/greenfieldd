801b98fe59182a320911644af94151a984837e2e
Rawan Bazadough <bazadough1997@gmail.com>
2020-12-04 19:29:57 +0200

    final version- uncommented

--- react-client/src/components/List.jsx
+++ react-client/src/components/List.jsx
@@ -1,27 +1,24 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
-import CardActionArea from '@material-ui/core/CardActionArea';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
-import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
-import { Grid } from '@material-ui/core';
-import GridList from '@material-ui/core/GridList';
-import $ from 'jquery';
-import ReactDOM from 'react-dom';
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardActionArea from "@material-ui/core/CardActionArea";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
+import { Grid } from "@material-ui/core";
+import GridList from "@material-ui/core/GridList";
+import $ from "jquery";
+import ReactDOM from "react-dom";
 
 export default class List extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
   }
   render() {
     return (
-      <Grid
-        container item
-        key={this.props.car.id}
-        xs={4}>
+      <Grid container item key={this.props.car.id} xs={4}>
         <Card>
           <CardActionArea>
             <CardMedia
@@ -33,15 +30,17 @@ export default class List extends React.Component {
             />
             <CardContent>
               <Typography gutterBottom variant="h5" component="h2">
-                {this.props.car.brand}     {" $"}{this.props.car.price}
+                {this.props.car.brand} {" $"}
+                {this.props.car.price}
               </Typography>
               <Typography variant="body2" color="textSecondary" component="p">
-                {this.props.car.description}   {". External body colour :"}{this.props.car.colour}
+                {this.props.car.description} {". External body colour :"}
+                {this.props.car.colour}
               </Typography>
             </CardContent>
           </CardActionArea>
         </Card>
       </Grid>
-    )
+    );
   }
 }
--- react-client/src/components/logotext.jsx
+++ react-client/src/components/logotext.jsx
@@ -1,27 +1,24 @@
-import React from 'react';
-import Typography from '@material-ui/core/Typography';
-import { styled } from '@material-ui/core/styles';
-import Grid from '@material-ui/core/Grid';
+import React from "react";
+import Typography from "@material-ui/core/Typography";
+import { styled } from "@material-ui/core/styles";
+import Grid from "@material-ui/core/Grid";
 
-
-
-const MyText = styled(Typography) ({
+const MyText = styled(Typography)({
   variant: "h3",
   margin: " -40px 200px 35px 490px",
-  fontStyle: 'normal',
+  fontStyle: "normal",
   fontSize: 35,
-  color : "#F9FBE7"
-})
-
+  color: "#F9FBE7",
+});
 
 const LogoText = () => {
-  return(
+  return (
     <Grid>
       <MyText component="h2" gutterBottom>
-      SELL ONE, BUY ANOTHER
+        SELL ONE, BUY ANOTHER
       </MyText>
     </Grid>
-  )
-}
+  );
+};
 
 export default LogoText;
